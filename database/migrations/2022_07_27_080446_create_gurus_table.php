09463fea51a417cffdd175bbb408d4bda30e0083
nabilafauziya <thalitaazahra02@gmail.com>
2022-07-27 16:36:10 +0700

    First Pengerjaan ProjectPKL

---
+++ database/migrations/2022_07_27_080446_create_gurus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGurusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gurus', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nip');
+            $table->string('nama');
+            $table->string('mata_pelajaran');
+            $table->enum('jenis_kelamin',['Laki-laki','Perempuan']);
+            $table->timestamps();
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gurus');
+    }
+}
---
+++ database/migrations/2022_07_27_080705_create_siswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nis');
+            $table->string('nama');
+            $table->enum('jenis_kelamin',['Laki-laki','Perempuan']);
+            $table->foreign('id_kelas')->references('id')->on('kelas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
